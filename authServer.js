2021ee571bb78c4c19520f5b4796ca2fe913453a
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-21 21:14:31 +0530

    completed authentication using jwt

---
+++ authServer.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken === null) return res.status(401);
+    if (!refreshTokens.includes(refreshToken)) return res.status(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403);
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.status(204);
+})
+
+app.post('/login', (req, res) => {
+    // Authenticate user
+
+    const username = req.body.username;
+    const user = { name: username };
+
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+}
+
+app.listen(4000, () => console.log('Listening to Auth server'));
--- server.js
+++ server.js
@@ -22,21 +22,13 @@ app.get('/users', authenticate, (req, res) => {
     res.json(users.filter(user => user.name === req.user.name))
 });
 
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const user = { name: username };
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
-});
-
 function authenticate(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token === null) res.status(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) res.status(403);
+        if (err) return res.status(403);
         req.user = user;
         next();
     });
