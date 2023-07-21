63517d748be59f5e3b8cf9bb012805e79975e365
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-21 19:48:12 +0530

    added basic jwt

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # auth-flow-framework
 In Auth flow framework, we've grasped all of the frameworks and tools that are required for user authorization and authentication process.
 
-## Token creation
+## Token creation for resource exchange
 * Enter into node terminal using the command `C:\Users\PATH>node`.
-* Generate two tokens
+* Generate following two tokens and store them in `.env` variable.
     1. First token for `ACCESS_TOKEN`
         ```javascript
         require('crypto').randomBytes(64).toString('hex');
         ```
         This will generate 64 characters long string token
-    2. Second token for `REFRESH_TOKEN`
+    2. Second token for `REFRESH_TOKEN`.
     Repeat the same as done in Step 1.
+
+## Basic authentication created using jwt
+* JWT auth flow
+    ```
+    client ---login(using credentials)--->server--->jwt(generated)--->jwt(sent to client)--->POST /users(using authorization header)--->server verification
+    ```
+    1. if no headers provided, status `401` is returned
+    2. if verification failed, status `403` is returned
+* We've used `authenticate` middleware to verify and respond to the client.
---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const users = [
+    {
+        "name": "Harsh Raj",
+        "id": 1,
+    },
+    {
+        "name": "Shaswika",
+        "id": 2
+    }
+];
+
+app.get('/users', authenticate, (req, res) => {
+    res.json(users.filter(user => user.name === req.user.name))
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+});
+
+function authenticate(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token === null) res.status(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) res.status(403);
+        req.user = user;
+        next();
+    });
+}
+app.listen(3000, () => console.log('Listening to Auth flow framework'));
