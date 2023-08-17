516d374291f0943b757bf0864f5ca2dc91d7ddf8
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-08-17 16:01:34 +0530

    added method checking

--- authServer.js
+++ authServer.js
@@ -11,10 +11,10 @@ let refreshTokens = [];
 
 app.post('/token', (req, res) => {
     const refreshToken = req.body.token;
-    if (refreshToken === null) return res.status(401).json({message : 'Unauthorized'});
-    if (!refreshTokens.includes(refreshToken)) return res.status(403).json({message : 'Forbidden'});
+    if (refreshToken === null) return res.status(401).json({ message: 'Unauthorized' });
+    if (!refreshTokens.includes(refreshToken)) return res.status(403).json({ message: 'Forbidden' });
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({message : 'Forbidden'});
+        if (err) return res.status(403).json({ message: 'Forbidden' });
         const accessToken = generateAccessToken({ name: user.name });
         res.json({ accessToken: accessToken });
     })
@@ -22,7 +22,7 @@ app.post('/token', (req, res) => {
 
 app.delete('/logout', (req, res) => {
     refreshTokens = refreshTokens.filter(token => token !== req.body.token);
-    return res.status(204).json({message : 'Success'});
+    return res.status(204).json({ message: 'Success' });
 })
 
 app.post('/login', (req, res) => {
@@ -32,14 +32,14 @@ app.post('/login', (req, res) => {
     const user = { name: username };
 
     const accessToken = generateAccessToken(user);
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    const refreshToken = jwt.sign({ user, allowedMethods: ['POST'] }, process.env.REFRESH_TOKEN_SECRET);
     refreshTokens.push(refreshToken);
 
     res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+    return jwt.sign({ user, allowedMethods: ['POST'] }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
 }
 
 app.listen(4000, () => console.log('Listening to Auth server'));
--- server.js
+++ server.js
@@ -25,10 +25,11 @@ app.get('/users', authenticate, (req, res) => {
 function authenticate(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    if (token === null) return res.status(401).json({message: 'Unauthorized'});
+    if (token === null) return res.status(401).json({ message: 'Unauthorized' });
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({message : 'Forbidden'});
+        if (err) return res.status(403).json({ message: 'Forbidden' });
+        if (!user.allowedMethods.includes(req.method)) return res.status(403).json({ error: 'Access denied for this method.' });
         req.user = user;
         next();
     });
